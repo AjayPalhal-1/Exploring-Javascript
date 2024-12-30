1d2de5458255e2cdc6587ae6f6a3d4f768b4af1e
Ajay Palhal <ajaypalhal63@gmail.com>
2024-12-30 23:44:30 +0530

    Operators Did

---
+++ DayWise/Day3_Operators/index1.js
@@ -0,0 +1,86 @@
+
+
+
+// let a =2;
+// let b =13;
+
+// console.log(a+b);
+// console.log(b%a)
+// // console.log(--b%a)
+// console.log(b%a--)
+// console.log(b%a)
+// console.log(b%a--)
+
+
+
+
+// let c = 10 ;
+// let d = 10 ;
+// let e = "ap"
+//   str = "ap"
+//   str = 10
+
+// console.log(c===d)
+// console.log(e===c)
+// console.log(str==e)
+// console.log(c===str)
+
+
+
+// let age = 11;
+
+// let status = age > 18 ? "I Can Vote " : "I Can not Vote "
+// console.log(status)
+
+
+// let ans  = ( true && true && false )
+// console.log(ans)
+
+
+
+// FALSEY  AND TRUTHY 
+// SHORT Circuity 
+
+let ans = ("false" || "ajay")
+ans = (12 &&22)
+// ans = (true && null)
+// ans = (true || null)
+// ans = (7 && null)
+// ans = (7 || null)
+// ans = (7 && 4)
+// ans = (4 || 7)
+
+
+
+
+// Bitwise oprator 
+
+
+
+// console.log(~(0))
+// console.log(~(3))
+// console.log(~(1))
+
+
+
+// Shifting oprator 
+
+
+let a = 10 
+console.log(a<<1)
+console.log(a<<2)
+
+console.log(a>>1)
+console.log(a>>2)
+
+
+
+
+
+
+
+
+
+
+
+
