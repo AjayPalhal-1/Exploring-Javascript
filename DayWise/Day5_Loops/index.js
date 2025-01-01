ed50e5b54fc06377c9fe8499719c079c098b55c3
Ajay Palhal <ajaypalhal63@gmail.com>
2025-01-02 01:18:22 +0530

    Day 5 Revised Loops

---
+++ DayWise/Day5_Loops/index.js
@@ -0,0 +1,60 @@
+
+
+
+// for( i =0; i<10;i++)
+// {
+//     console.log("AJAY");
+// }
+
+
+// for(let i =1; i<= 10;i++)
+// {
+//     console.log(i)
+// }
+
+
+// for(let i=5; i>= 0;i--)
+//     {
+//         console.log(i)
+//     }
+
+
+    // for(let i =0; i<=10;i++)
+    // {
+    //     if(i==5)
+    //     {
+    //         break;
+    //     }
+    //     else{
+    //         console.log(i)
+    //     }
+    // }
+
+
+
+
+    // for(let i =0; i<=10;i++)
+    //     {
+    //         if(i==5)
+    //         {
+    //             continue;
+    //         }
+    //         else{
+    //             console.log(i)
+    //         }
+    //     }
+
+
+    // let i = 1;
+    // while(i<=10)
+    // {
+    //     console.log("Sat Saheb")
+    //     i++;
+    // }
+
+    let i = 10;
+    while(i>=1)
+    {
+        console.log("Sat Saheb")
+        i--;
+    }
