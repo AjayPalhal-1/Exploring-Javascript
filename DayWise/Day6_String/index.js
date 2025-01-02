d2bb47e92701ba9e77c69e88d75324d45d4e1abd
Ajay Palhal <ajaypalhal63@gmail.com>
2025-01-02 22:45:45 +0530

    String Strted

---
+++ DayWise/Day6_String/index.js
@@ -0,0 +1,39 @@
+
+
+// let str1 = "Ajay"
+// let str2 = "Palhal"
+// str3 = str1 +str2
+
+// console.log(str3)
+
+
+// console.log(typeof(str1))
+// console.log(typeof(str2))
+
+let name = new String("AJAY_PALHAL")
+// name.String
+console.log(typeof(name))
+// console.log(typeof( *name))
+
+
+
+let myname = `Don`
+console.log(myname)
+
+
+
+let str1 = "Ajay PALHAL"
+let str2 = "Palhal"
+str3 = `${str1} ${str2}`
+
+console.log(str3)
+console.log(str1.length)
+console.log(str1.toLowerCase)
+console.log(str1.toUpperCase)
+console.log(str2.concat)
+console.log(str1.split)
+
+
+
+
+
