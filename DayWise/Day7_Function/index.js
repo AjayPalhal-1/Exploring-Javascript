2573a9532d3fe2334202cc243cc3fbe59bd4cd07
Ajay Palhal <ajaypalhal63@gmail.com>
2025-01-04 00:08:35 +0530

    Fuction Explored

---
+++ DayWise/Day7_Function/index.js
@@ -0,0 +1,79 @@
+
+
+// str = "Your Growing Bro Keep it Up !!!"
+// let num = 100
+
+// function myName( str ,num)
+// {
+//     for(let i =0;i<10;i++)
+//     {
+//         console.log(str ,num );
+
+//     }
+// }
+// myName(str ,num)
+
+
+
+
+// {
+
+//  str = "Your Growing Bro Keep it Up !!!"
+//     num = 100
+
+
+// }
+
+// function myName( )
+// {
+//     for(let i =0;i<10;i++)
+//     {
+//         console.log( num ,str);
+
+//     }
+// }
+// myName(num ,str)
+
+
+
+
+
+// let a=10
+// let b =10.0
+
+// function   myFunc (a ,b)
+// {
+//     return "Learning Javascript "
+// }
+
+// console.log(myFunc(a ,b))
+
+
+
+// a =10 
+// b=10
+
+// let  mul  = function (a,b)
+// {
+//     return a *b;
+// }
+// console.log(mul(a,b))
+
+{a= 12
+b= 12
+
+let value =  (a,b) => a*b
+// {
+//     return  a*b
+// }
+
+console.log(value(a,b))
+}
+
+
+c =120 
+d=20
+
+let mynum = (c,d)=> c-d
+
+console.log(mynum(c,d))
