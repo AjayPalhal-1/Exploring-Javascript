423b8ed9c76d6a9e270e1c73a0ab0cd8cb53e9eb
Ajay Palhal <ajaypalhal63@gmail.com>
2025-01-08 11:07:46 +0530

    Function Hoisting Explored

---
+++ DayWise/Day11_var_func_Hoisting/index.js
@@ -0,0 +1,43 @@
+
+
+
+// printMyName();
+
+// function printMyName()
+// {
+//     console.log("AJAY PALHAL ")
+// }
+
+
+// console.log(age)
+// var age = 23;
+
+
+
+
+// hoisting will not work like normal function 
+
+// myfun()
+
+// let myfun =  function ()
+// {
+//     console.log("Hello Ji Namste ")
+// }        
+
+
+// newfun = function()
+// {
+//     console.log("hiii")
+// }
+//  newfun()
+
+
+
+class Home {
+
+     
+}
+
+obj1 = new Home()
+
+console.log(obj1)
