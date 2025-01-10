935cb9d319c6157386e1a9944ed2b4c8a9c1a055
Ajay Palhal <ajaypalhal63@gmail.com>
2025-01-10 14:50:32 +0530

    TDZ Explored

---
+++ DayWise/Day13_Temporal_Dead_Zone/index.js
@@ -0,0 +1,70 @@
+
+
+// Gloabal Scope 
+
+// var age =23;
+// let age = 24;
+// const age = 24;
+
+
+// {
+//     console.log(age)
+// }
+
+// if(age)
+// {
+//     console.log(age)
+// }
+
+// ageprint()
+// function ageprint()
+// {
+//     console.log("In Function",age)
+// }
+
+
+// Function Scope 
+
+
+// function sayhi()
+// {
+//     console.log("Hlw everyOne")
+//     let  name = "Ajay"
+//     console.log(name)
+// }
+
+// sayhi()
+//  console.log(name)
+
+
+/// Block Level Scope 
+
+
+// {
+//     var myname = "AJAY"
+//     const  sirname = "PALHAL"
+//     let rollno = 99
+// }
+
+// console.log(myname)
+// console.log(sirname)
+// console.log(rollno)
+
+
+
+
+// Temporal Dead Zone 
+
+// console.log(age)
+// console.log("HIIII")
+// console.log("helooo!1!!!!!")
+
+
+// var age = 23;
+
+console.log(age1)
+console.log("HIIII")
+console.log("helooo!1!!!!!")
+
+
+const age1 = 23;
