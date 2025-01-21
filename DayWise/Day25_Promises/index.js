6f0b8c1626bd075554c9351ce9b591caff3a868c
Ajay Palhal <ajaypalhal63@gmail.com>
2025-01-21 21:16:40 +0530

    Promise Object Exploring..!

---
+++ DayWise/Day25_Promises/index.js
@@ -0,0 +1,13 @@
+// let  firstpromise = new Promise((resolve , reject)=>
+//     {
+//         console.log("Ajay Palhal");
+//          reject(new Error("Internal Error Bro !!!"));
+//     });
+
+
+    function printmyName()
+    {
+        console.log("Call Me AJay Vrooo!!!!")
+    }
+
+    setTimeout(printmyName, 10000);
